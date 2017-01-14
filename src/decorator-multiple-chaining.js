e6bfea85de8bb28f4d0aa229c812f1b8867af502
Jerry Chang <therealjerrycan@gmail.com>
2017-01-14 15:02:25 -0800

    added additional feature for multiple decorators to be chained

---
+++ src/decorator-multiple-chaining.js
@@ -0,0 +1,42 @@
+import { prettyPrintJSON } from './util'
+
+function MacBook() {
+    this.cost = function() { return 997 }
+    this.screenSize = function() { return 11.6 }
+}
+
+/* first decorator */
+function memory(macbook) {
+    var v = macbook.cost()
+    macbook.cost = function() { return v + 75 }
+    return macbook
+}
+
+/* second decorator */
+function engraving(macbook) {
+    var v = macbook.cost()
+    macbook.cost = function() { return v + 200 }
+    return macbook
+}
+
+
+/* third decorator */
+function insurance(macbook) {
+    var v = macbook.cost()
+    macbook.cost = function() { return v + 250 }
+    return macbook
+}
+
+var mb = new MacBook()
+console.log('total base cost %s', mb.cost())
+
+/* Additional features (decorators) */
+memory(engraving(insurance(mb)))
+
+console.log('total cost with memory, engraving and insurance: %s', mb.cost())
+
+for (let k in mb) {
+    console.log(mb[k].toString())
+}
+
+
